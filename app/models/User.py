ef67c58c3469a1cc7ccc1a62a4567f16aca65684
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-09 17:11:25 -0700

    Add email & password validation

--- app/models/User.py
+++ app/models/User.py
@@ -3,9 +3,21 @@ from sqlalchemy.sql.expression import null
 from app.db import Base
 from sqlalchemy import Column, Integer, String
 
+
 class User(Base):
-  __tablename__ = 'users'
-  id = Column(Integer, primary_key=True)
-  username = Column(String(50), nullable=False)
-  email = Column(String(50), nullable=False, unique=True)
-  password = Column(String(100), nullable=False)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        assert '@' in email
+        return email
+
+    @validates('password')
+    def validate_password(self, key, password):
+        assert len(password)
+        return password
