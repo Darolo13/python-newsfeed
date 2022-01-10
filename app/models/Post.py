acffcdbca481f96ffaf7acdafb7c15220d98a77e
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-09 18:21:50 -0700

    Create Post Model

---
+++ app/models/Post.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import sqlalchemy
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.operators import collate
+from sqlalchemy.sql.sqltypes import Date
+from app.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user = relationship('User')
